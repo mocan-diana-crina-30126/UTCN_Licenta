a6a7e5ce6c08348b87401571ffb15cf1864e5f5d
Mocan Diana <mocan.diana2013@gmail.com>
2022-04-13 16:02:58 +0300

    Adaugarea functionalitatii butonului de play a filmelor din cadrul componentei slider (pagina home)

--- FE/MovApp-frontend/src/app/main-page/components/slider/slider.component.ts
+++ FE/MovApp-frontend/src/app/main-page/components/slider/slider.component.ts
@@ -20,28 +20,7 @@ public id!: number;
 constructor(private router: ActivatedRoute, private movieService: MovieService, private routerr: Router) { }
 
   ngOnInit(): void {
-    this.router.params.subscribe((params: Params) => {
-      this.id = params['id'];
-      console.log(this.id);
-      this.routerr.navigate(['/video', this.id]);
-    });
-    this.getMovieContent(this.id);
+
   }
-
-public displayVideo(){
-
-
-    this.getMovieContent(this.id);
-
-
-  // console.log('PLAY!');
-  // this.router.navigate(['/video'], {relativeTo: this.route});
-}
-
-getMovieContent(id: any){
-  this.movieService.getMovieContent(id).subscribe((data => { this.content = data; console.log(this.content) ;}));
-  console.log(id);
-  //this.routerr.navigate(['/video', id]);
-}
 
 }
---
+++ FE/MovApp-frontend/src/app/main-page/components/slider/slider.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatButtonModule} from "@angular/material/button";
+
+import {SliderComponent} from "./slider.component";
+import {SliderRoutingModule} from "./slider-routing.module";
+
+@NgModule({
+  declarations: [SliderComponent],
+  imports: [
+    CommonModule,
+    SliderRoutingModule,
+    MatButtonModule
+  ]
+})
+
+export class SliderModule { }
